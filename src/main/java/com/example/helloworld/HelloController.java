0c5166875335391fb6e889434db0d952b40ef6e5
Shenali30 <shenalik30@gmail.com>
2021-07-08 17:43:28 +0530

    Added custom hello message

--- src/main/java/com/example/helloworld/HelloController.java
+++ src/main/java/com/example/helloworld/HelloController.java
@@ -1,6 +1,7 @@
 package com.example.helloworld;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,5 +16,11 @@ public class HelloController {
     @GetMapping("/name")
     public String getName(){
         return "Anderson";
+    }
+
+    @GetMapping("/myhello")
+    public String customHello(@RequestParam("name") String name){
+        return "Hello " + name;
+
     }
 }
